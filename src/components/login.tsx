ccfa54d97a62e8dc1adc58f82a50fdfef8fa192f
MarcMalha <marc.malha@beanstock.com>
2025-06-04 22:22:53 +0200

    Style: improve login form styling

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from "react";
 import api from "@/utils/api";
+import {AxiosError, isAxiosError} from "axios";
 
 export default function LoginForm() {
     const [formData, setFormData] = useState({
@@ -21,15 +22,17 @@ export default function LoginForm() {
         const email = form.children.namedItem('email') as HTMLInputElement;
         const password = form.children.namedItem('password') as HTMLInputElement;
 
-        console.log(email.value);
-        console.log(password.value);
         try {
             const response = await api.post('/me', {
                 "email": email.value,
                 "password": password.value,
             });
-        } catch (e) {
-            console.log(e.status);
+
+
+        } catch (e: unknown) {
+            const axiosError = isAxiosError(e) && e as AxiosError;
+
+            console.log(axiosError && axiosError.status);
         }
 
     }
@@ -37,9 +40,9 @@ export default function LoginForm() {
     return (
         <div className="flex items-center justify-center min-h-screen border-solid bg-foreground">
             <form onSubmit={handleSubmit}
-                  className="p-6 border border-gray-400 rounded-md bg-white shadow-md">
+                  className="relative flex flex-col items-center gap-2 p-6 border border-gray-400 rounded-md bg-white shadow-md w-[350px] h-[450px]">
                 <h1
-                    className="text-3xl font-bold text-base text-center mb-4 text-black"
+                    className="text-3xl font-bold text-base text-center mb-4 mt-4 text-black"
                 >
                     Login
                 </h1>
@@ -48,7 +51,7 @@ export default function LoginForm() {
                     type="email"
                     name="email"
                     required
-                    className="rounded border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                    className="rounded border-transparent transition-colors block w-64 mb-2 bg-foreground text-background text-center hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
                     placeholder="example@email.com"
                     onChange={handleChange}
                     value={formData.email}
@@ -58,14 +61,14 @@ export default function LoginForm() {
                     type="password"
                     name="password"
                     required
-                    className="rounded border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                    className="rounded border-transparent transition-colors block w-64 mb-2 bg-foreground text-background text-center hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
                     placeholder="password"
                     onChange={handleChange}
                     value={formData.password}
                 />
                 <br/>
                 <a
-                    className="text-blue-300"
+                    className="text-blue-300 block mx-auto mb-4 text-center"
                     href='register'
                 >
                     Don't have an account?
@@ -74,9 +77,9 @@ export default function LoginForm() {
                 <br/>
                 <button
                     type="submit"
-                    className="ml-auto rounded border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                    className="absolute bottom-10 right-10 ml-auto rounded border-solid border-transparent transition-colors flex items-center justify-center  bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
                 >
-                    Submit
+                    Login
                 </button>
             </form>
         </div>
