2921affa90eb3d71bb2c72069ae35ad3f3731361
badis <badis.tighlit@gmail.com>
2025-07-02 20:06:16 +0200

    incrust logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,5 +2,12 @@
 import Login from "./components/LoginForm";
 
 export default function LoginPage() {
-    return <Login />;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white p-6">
+      {/* Logo */}
+      <img src="/logo-transparent.png" alt="Zaptask Logo" className="mb-6 h-12 w-auto" />
+
+      <Login />
+    </div>
+  );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 import RegisterForm from "./components/RegisterForm";
 export default function RegisterPage() {
-    return <RegisterForm />;
+      return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-white p-6">
+          {/* Logo */}
+          <img src="/logo-transparent.png" alt="Zaptask Logo" className="mb-6 h-12 w-auto" />
+    
+          <RegisterForm />
+        </div>
+      );
 }
